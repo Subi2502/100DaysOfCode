6638f93cfabd14a0c0a53243c6f532ec070780fc
Subi <shinisubha2511@gmail.com>
2023-09-20 23:26:28 +0530

    add content for Day1 file

---
+++ Day1_python.py
@@ -0,0 +1,30 @@
+# Print "Hello, world!" to the console 
+print ("\nSubi - Day 1 of #100DaysOfCode\n")
+
+# Accept user input and print it. 
+print("Program to get Input and print Output\n")
+name = input("Enter name: ")
+age = input("Enter age: ")
+print("My Name is:",name)
+print("My Age is:",age)
+
+# Calculate the sum of two numbers 
+print("\nAddition program")
+num1 = 5
+num2 = 10
+addition = num1 + num2
+print(addition)
+
+# Calculate the product of two numbers 
+print("\nProduct program")
+num3 = 5
+num4 = 10
+multiplication = num3*num4
+print(multiplication)
+
+# Find the square root of number. 
+print("\nFinding square root program")
+import math
+x = 25
+math.sqrt(x)
+print(x)
