5eee79ee9ee3cfdf0d8f3b53960066342de36c78
Subi <shinisubha2511@gmail.com>
2023-09-27 18:55:35 +0530

    add day8 program

---
+++ Day8_python.py
@@ -0,0 +1,6 @@
+#Find the largest element in a list
+print ("\nSubi - Day 8 of #100DaysOfCode\n")
+print("\nlargest element in a list\n")
+
+List = [25, 64, 86, 95, 96, 74, 53]
+print("largest element is:", max(List))
