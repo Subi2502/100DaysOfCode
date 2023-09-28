5666cc2cab3772b22f4084167c90425165be362a
Subi <shinisubha2511@gmail.com>
2023-09-28 20:54:53 +0530

    add day_9 program

---
+++ Day9_python.py
@@ -0,0 +1,12 @@
+#Sort a list of numbers in ascending and descending order. 
+print ("\nSubi - Day 9 of #100DaysOfCode\n")
+print("\nlist of numbers in ascending\n")
+
+a = ( 1, 34, 97, 56, 86, 45, 9, 32, 48)
+x = sorted(a)
+print(x)
+
+print("\nlist of numbers in descending\n")
+a = (1, 34, 97, 56, 86, 45, 9, 32, 48)
+x = sorted(a, reverse=True)
+print(x)
