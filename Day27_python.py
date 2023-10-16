a6b7810959c30ca178a68baec44ff83224efa114
Subi <shinisubha2511@gmail.com>
2023-10-16 10:22:16 +0530

    add day_27 program

---
+++ Day27_python.py
@@ -0,0 +1,42 @@
+#Implement a stack data structure using a list
+# print ("\nSubi - Day 27 of #100DaysOfCode\n") 
+# print("\nImplement a stack data structure using a list\n")
+
+# stack = []
+
+# stack.append("10")
+# stack.append("20")
+# stack.append("30")
+
+# print("\nInitial stack")
+# print(stack)
+
+# print("\nNumbers popped from stack: ")
+# print(stack.pop())
+# print(stack.pop())
+# print(stack.pop())
+
+# print("\nStack after Numbers are popped: ")
+# print(stack)
+
+
+#Create a queue data structure using a list
+print("\nCreate a queue data structure using a list\n")
+
+queue = []
+
+queue.append("10")
+queue.append("20")
+queue.append("30")
+
+print("\nInitial queue")
+print(queue)
+
+print("\nNumber dequeued form queue")
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
+
+print("\nQueue after removing Numbers")
+print(queue)
+
