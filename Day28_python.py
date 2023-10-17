a53a04505749b254180eaf10570a66b7f7dc093e
Subi <shinisubha2511@gmail.com>
2023-10-17 21:10:14 +0530

    add day_28 img

--- Day28_python.py
+++ Day28_python.py
@@ -1,29 +1,28 @@
 #Write a program to count the frequency of elements in a list using a dictionary
-# print ("\nSubi - Day 28 of #100DaysOfCode\n") 
-# print("\nprogram to count the frequency of elements in a list using a dictionary\n")
+print ("\nSubi - Day 28 of #100DaysOfCode\n") 
+print("\nprogram to count the frequency of elements in a list using a dictionary\n")
 
-# def CountFreq(list):
-#   freq = {}
-#   for items in list:
-#     freq[items] = list.count(items)
-#   print(freq)
+def CountFreq(list):
+  freq = {}
+  for items in list:
+    freq[items] = list.count(items)
+  print(freq)
 
-# list =['a', 's', 'a', 's', 'c', 'c', 'c','b']
-# CountFreq(list)
+list =['a', 's', 'a', 's', 'c', 'c', 'c','b']
+CountFreq(list)
 
 
 #Implement a binary search algorithm to find an element in a sorted list.
 print("\na binary search algorithm to find an element in a sorted list\n")
 
-def binarySearch(array, target):
-    left, right = 0, len(array)-1
+def binary_search(lst, target):
+    left, right = 0, len(lst) - 1
     while left <= right:
-        middle = (left+right) // 2
-        potentialMatch = array[middle]
-        if target == potentialMatch:
-            return middle
-        elif target < potentialMatch:
-            right = middle - 1
+        mid = left + (right - left) // 2
+        if lst[mid] == target:
+            return mid
+        elif lst[mid] < target:
+            left = mid + 1
         else:
-            left = middle + 1
+            right = mid - 1
     return -1
