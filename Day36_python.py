1b570031c8952a2850306daa4ef0acc61cd43df2
Subi <shinisubha2511@gmail.com>
2023-10-23 17:23:30 +0530

    add day_36 program

---
+++ Day36_python.py
@@ -0,0 +1,31 @@
+#Write a program that handles exceptions for division by zero.
+print ("\nSubi - Day 36 of #100DaysOfCode\n") 
+print("\na program that handles exceptions for division by zero\n")
+
+def zero_exception(x, y):
+    try:
+        return(x / y)
+    except ZeroDivisionError:
+        print("Error: dividing by zero")
+        return None
+    
+print(zero_exception(15, 8))
+print(zero_exception(15, 0))
+
+
+#Implement a try-except block to catch a specific exception.
+print("\nImplement a try-except block to catch a specific exception\n")
+
+# try:
+#     user_input = input("Enter a number: ")
+#     number = int(user_input)
+# except ValueError:
+#     print("Invalid input. Please enter a valid number.")
+
+
+try:
+    file = open("myfile.txt")
+    contents = file.read()
+    file.close()
+except FileNotFoundError:
+    print("Error: File does not exist.")
