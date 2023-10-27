47855a52a323cc6dc6988a0abbadb433099e0d31
Subi <shinisubha2511@gmail.com>
2023-10-27 18:12:49 +0530

    add day_41 program

---
+++ Day41_python.py
@@ -0,0 +1,30 @@
+#Create a generator function that generates Fibonacci numbers.
+# print ("\nSubi - Day 41 of #100DaysOfCode\n") 
+# print("\nCreate a generator function that generates Fibonacci numbers\n")
+
+# def fibonacci():
+#     a, b = 0, 1
+#     while True:
+#         yield a
+#         a, b = b, a + b
+
+# fib = fibonacci()
+# for _ in range(5):
+
+#     print(next(fib))
+
+import re
+
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+')
+
+def isValid(email):
+    if re.fullmatch(regex, email):
+      print("Valid email")
+    else:
+      print("Invalid email")
+
+isValid("subiii.subi@gmail.com")
+isValid("subi123@yahoo.co.uk")
+isValid("subii123@...uk")
+isValid("...@domain.us")
+      
