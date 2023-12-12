2f1cb61c518b359b53288569008eb98025f773d5
Subi <shinisubha2511@gmail.com>
2023-12-12 21:49:22 +0530

    add day_84 program

---
+++ Day84_ython.py
@@ -0,0 +1,43 @@
+#.
+print ("\nSubi - Day 84 of #100DaysOfCode\n") 
+print("\n\n")
+
+class TrieNode:
+ 
+    def __init__(self, char):
+        self.char = char
+        self.is_end = False
+        self.children = {}
+ 
+class Trie(object):
+ 
+    def __init__(self):
+        self.root = TrieNode("")
+     
+    def insert(self, word):
+        node = self.root
+        for char in word:
+            if char in node.children:
+                node = node.children[char]
+            else:
+                new_node = TrieNode(char)
+                node.children[char] = new_node
+                node = new_node 
+        node.is_end = True
+         
+    def dfs(self, node, pre):
+        if node.is_end:
+            self.output.append((pre + node.char)) 
+        for child in node.children.values():
+            self.dfs(child, pre + node.char)
+         
+    def search(self, x):
+        node = self.root 
+        for char in x:
+            if char in node.children:
+                node = node.children[char]
+            else:      
+                return [] 
+        self.output = []
+        self.dfs(node, x[:-1])
+        return self.output
